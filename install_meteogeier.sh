b2d54e29e376597746d7cfd4fd13cf128c399777
Maxi Padulo <maxi.padulo@gmail.com>
2016-03-18 14:27:47 +0100

    ref #8, setting up missing directories

--- install_meteogeier.sh
+++ install_meteogeier.sh
@@ -9,5 +9,9 @@ apt-get install -y $PACKAGES $AUDIO_GENERATION
 locale-gen de_DE
 locale-gen de_DE.UTF-8
 
+# Perl dependencies
 export PERL_MM_USE_DEFAULT=1 
 perl -MCPAN -e'install "LWP::Simple"'
+
+# Set up directories
+mkdir -p /var/spool/voice/messages
