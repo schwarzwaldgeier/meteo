883e057396c00e539345a6813e7e84beee765bb8
Maxi Padulo <maxi.padulo@gmail.com>
2016-03-18 13:41:57 +0100

    ref #8, be verbose by linking /var/www

--- bootstrap_web_server.sh
+++ bootstrap_web_server.sh
@@ -5,6 +5,7 @@ PACKAGES='apache2 git vim'
 
 apt-get update && apt-get install -y $PACKAGES
 if ! [ -L /var/www ]; then
+  echo 'INFO: It seems /var/www is not right. Linking /var/www to /vagrant ...'
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
