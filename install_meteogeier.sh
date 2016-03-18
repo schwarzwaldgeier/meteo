ca3662c97120d1d1f9ba402ea0ecb66f86ee2758
Maxi Padulo <maxi.padulo@gmail.com>
2016-03-18 14:21:08 +0100

    ref #8, adding shntool to install

--- install_meteogeier.sh
+++ install_meteogeier.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PACKAGES='mysql-server mysql-client '
-AUDIO_GENERATION='sox mgetty-pvftools'
+AUDIO_GENERATION='sox mgetty-pvftools shntool'
 
 apt-get install -y $PACKAGES $AUDIO_GENERATION
 
