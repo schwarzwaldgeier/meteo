163fd64de7dccd53c7d1ee040aa7814af635119f
Maxi Padulo <maxi.padulo@gmail.com>
2016-03-18 13:24:51 +0100

    ref #8, fixing locales

--- install_meteogeier.sh
+++ install_meteogeier.sh
@@ -3,7 +3,7 @@
 PACKAGES='mysql-server mysql-client sox'
 
 apt-get install -y $PACKAGES
-#if ! [ -L /var/www ]; then
-  #rm -rf /var/www
-  #ln -fs /vagrant /var/www
-#fi
+
+# Install locales (DE)
+sudo locale-gen de_DE
+sudo locale-gen de_DE.UTF-8
