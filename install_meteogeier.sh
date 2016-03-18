b9debfc5c5c05d8848c5d390182c43de7212e184
Maxi Padulo <maxi.padulo@gmail.com>
2016-03-18 13:04:58 +0100

    ref #8, vagrant bootstrap files

---
+++ install_meteogeier.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+WWW_USER=vagrant
+
+PACKAGES='apache2 git vim'
+
+apt-get install -y $PACKAGES
+#if ! [ -L /var/www ]; then
+  #rm -rf /var/www
+  #ln -fs /vagrant /var/www
+#fi
