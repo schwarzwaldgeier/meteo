6d21d43a34fbaed1584194af01d8de276d68ce26
Maximiliano Padulo <maxi.padulo@gmail.com>
2016-08-01 09:21:41 +0200

    ref #20, skip recovery of deprecated vigor router
    Vigor router now out of service

--- BERGSTATION/monitor/wlan_router_in_den_arsch_tretten.sh
+++ BERGSTATION/monitor/wlan_router_in_den_arsch_tretten.sh
@@ -5,7 +5,7 @@ echo "[router_in_den_arsch_tretten] Started"
 echo "INFO: Loading sensitive pasword for router access"
 set -e; source /var/www/.sensitive; set +e  
 
-#echo "INFO: Restarting WLAN on router..."
+echo "SKIPPING: Restarting WLAN on router... Vigor router not present anymore"
 ##TODO: check output and search for matching string to detect if telnet session
 ## worked or not
 #(sleep 1; echo $ROUTER_PW; echo "wl act off"; sleep 5; echo "wl act on"; sleep 2; echo exit; sleep 1)|telnet $ROUTER_IP
