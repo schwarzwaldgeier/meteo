86ef70c87ce15d75c03463934e8c0a34fd1cb129
Sebastian Schmied <s.schmied@gmail.com>
2016-09-15 14:04:29 +0200

    kph to knots, timestamp as readable date+time

--- WEBROOT/wetterstation/windfinder.php
+++ WEBROOT/wetterstation/windfinder.php
@@ -1,31 +1,19 @@
 <?php
 
 @require_once($_SERVER["DOCUMENT_ROOT"]."/inc/php_mysql.php");
-
-
-
-
 $sql = "SELECT uid, tstamp, temperature, wind_speed, wind_maxspeed, wind_direction, pressure, humidity FROM weather_merkur2 ORDER BY tstamp DESC LIMIT 100";
 $result = mysql_query($sql);
-
-
-
-
-
-
-
 echo "Date\tTime\tAirTemp\tWindspeed\tGusts\tWindDir\tBarometer\tHumidity\r\n";
 
 while ($row = mysql_fetch_assoc($result)) {
-    echo $row["tstamp"]."\t";
+    echo date("d.m.Y",$row["tstamp"])."\t"; 
+	echo date("H:i",$row["tstamp"])."\t";
     echo $row["temperature"]."\t";
-    echo $row["wind_speed"]."\t";
-	echo $row["wind_maxspeed"]."\t";
+    echo string(floatval(($row["wind_speed"])*0.539957)."\t"; 		//kph 
+	echo string(floatval(($row["wind_maxspeed"])*0.539957)."\t";	//to knots
 	echo $row["wind_direction"]."\t";
 	echo $row["pressure"]."\t";
 	echo $row["humidity"]."\r\n";
-	
-	
 }
 
 
