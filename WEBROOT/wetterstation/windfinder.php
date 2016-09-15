3ffdcaaa801f5000440982ba21b9914d2d82419f
Sebastian Schmied <s.schmied@gmail.com>
2016-09-15 13:47:49 +0200

    output test

--- WEBROOT/wetterstation/windfinder.php
+++ WEBROOT/wetterstation/windfinder.php
@@ -1,21 +1,32 @@
 <?php
-$anzkomplett = 0;
+
 @require_once($_SERVER["DOCUMENT_ROOT"]."/inc/php_mysql.php");
 
 
 
-function data_avg($mins) {
 
-    	$query = "SELECT uid, tstamp, temperature, wind_speed, wind_maxspeed, wind_direction, pressure, humidity FROM weather_merkur2 WHERE tstamp > $mytime";
-    	
+$sql = "SELECT uid, tstamp, temperature, wind_speed, wind_maxspeed, wind_direction, pressure, humidity FROM weather_merkur2 ORDER BY tstamp DESC LIMIT 100";
+$result = mysql_query($sql);
+
+
+
+
+
+
+
+echo "Date\tTime\tAirTemp\tWindspeed\tGusts\tWindDir\tBarometer\tHumidity\r\n";
+
+while ($row = mysql_fetch_assoc($result)) {
+    echo $row["tstamp"]."\t";
+    echo $row["temperature"];."\t";
+    echo $row["wind_speed"];."\t";
+	echo $row["wind_maxspeed"];."\t";
+	echo $row["wind_direction"];."\t";
+	echo $row["pressure"];."\t";
+	echo $row["humidity"];."\r\n";
+	
+	
 }
-
-
-
-
-
-echo 'Date \t Time \t AirTemp \t MaxTemp \t Windspeed \t Gusts \t WindDir \t Barometer \t Humidity\r\n';
-
 
 
 
