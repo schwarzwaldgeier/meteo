f9e67200b1e0addbc6ac9a5791dbbf842521911e
Sebastian Schmied <s.schmied@gmail.com>
2016-09-15 16:59:24 +0200

    Use uncompensated data. There's no need to try and get takeoff wind values when providing data for the general public. See #ref 31

--- WEBROOT/wetterstation/windfinder.php
+++ WEBROOT/wetterstation/windfinder.php
@@ -1,14 +1,15 @@
 <?php
 ini_set('display_errors', 1);
 @require_once($_SERVER["DOCUMENT_ROOT"]."/inc/php_mysql.php");
-$sql = "SELECT uid, tstamp, temperature, wind_speed, wind_maxspeed, wind_direction, pressure FROM weather_merkur2 ORDER BY tstamp DESC LIMIT 1440"; //1440 = 24h
+$sql = "SELECT uid, tstamp, temperature, original_wind_speed, original_wind_maxspeed, wind_direction, pressure FROM weather_merkur2 ORDER BY tstamp DESC LIMIT 1440"; //1440 = 24h
 $result = mysql_query($sql);
+$
 echo "Date\tTime\tAirTemp\tWindspeed\tGusts\tWindDir\tBarometer\r\n";
 
 while ($row = mysql_fetch_assoc($result)) {
 	
-	$windspeed_knots = floatval($row["wind_speed"])*0.539957;
-	$gust_knots = floatval($row["wind_maxspeed"])*0.539957;
+	$windspeed_knots = floatval($row["original_wind_speed"])*0.539957;
+	$gust_knots = floatval($row["original_wind_maxspeed"])*0.539957;
 	
     echo date("d.m.Y",$row["tstamp"])."\t"; 
 	echo date("H:i",$row["tstamp"])."\t";
