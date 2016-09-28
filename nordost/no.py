6c9a7eeb9ef768fc93a6eb7fa2dd6ec22d901679
sebschmied <sebastian.schmied@1und1.de>
2016-09-28 22:01:58 +0200

    remove old code and surplus whitespace

--- nordost/no.py
+++ nordost/no.py
@@ -12,47 +12,27 @@ if sys.version_info >= (3,0):
 else:
     from urllib import urlopen
 
-
 max_windspeed = 5
 wind_direction_boundaries = {'north': 17.5, 'east': 62.5}
 #wind_direction_boundaries = {'north': 0, 'east': 360} #TEST ONLY!
 
-
-
-	
-
-		
 def read_sensitive(path):
-   # #print (path)
     sensitive = {}
     txt = open(path).read()
-    ##print(txt)
     txt = txt.strip()
     lines = txt.splitlines()
     for line in lines:
         if len(line) > 0 and line[0] != '#' and '=' in line: #not a comment, not empty, has a '=', doesn't suck
             line = line.replace("export ", '')
             entry = line.split('=')
-            ##print (entry)
-            sensitive[entry[0]] = entry[1]
-        
+            sensitive[entry[0]] = entry[1]    
     return sensitive
-        
-        
-    
-    
-    
-    
-    
-    
-    
-		
+
 def is_windspeed_ok(forecast):
 	wind_speed = forecast['wind']['speed']
 	if wind_speed <= max_windspeed:
 		return True
 	else:
-		#print ("Wind too strong: %s km/h" % wind_speed * 3.6)
 		return False
 		
 def is_wind_direction_ok(forecast):
@@ -62,7 +42,6 @@ def is_wind_direction_ok(forecast):
 	if north <= wind_direction <= east:
 		return True
 	else:
-		#print ("Bad wind direction: %s°" % wind_direction)
 		return False
 	
 def is_weekend(forecast):
@@ -70,21 +49,18 @@ def is_weekend(forecast):
 	if date.weekday() >= 5:
 		return True
 	else:
-		#print ("Not a weekend: %s" % date.strftime("%A"))
 		return False;
 		
 def is_dry(forecast):
 	if not forecast['rain']: # would contain downpour in l/m2 otherwise
 		return True
 	else:
-		#print ("Rain: %smm" % forecast['rain']['3h'])
 		return False
 	
 def is_sunny(forecast):
 	if int(forecast['clouds']['all']) <= 75: #=6/8 clouds max
 		return True
 	else:
-		#print ("Too cloudy: %s%%" % int(forecast['clouds']['all']))
 		return False
 	
 	
@@ -93,7 +69,6 @@ def is_during_day(forecast):
 	if 10 <= date.hour <= 17:
 		return True
 	else:
-		#print ("Too late or early: %s:00" % date.hour)
 		return False
 	
 def is_good_time_for_briefing(forecast):
@@ -101,13 +76,11 @@ def is_good_time_for_briefing(forecast):
 		return True
 	else:
 		return False
-	
-
 
 def announce_briefing_day(forecast):
 
 	formatted_forecasts = ""
-	German_weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag']
+	German_weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'] #yes i know there is something built in for this but it looked complicated and I didn't care
 	for forecast in forecast:
 		godddate = date.fromtimestamp(forecast['dt'])
 		windspeed = str(int(round(forecast['wind']['speed'] * 3.6)))
@@ -119,14 +92,8 @@ def announce_briefing_day(forecast):
 	emailbody = emailbody.replace('{forecasts}', formatted_forecasts)
 	emailbody = emailbody.replace('{participantsurl}', sensitive['SENSITIVE_NORTHEAST_LISTURL'])
 	emailbody = emailbody.replace('{mailinglist}', sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'])
-	
-	
 	send_email(sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], "Nordosteinweiser für's Wochenende gesucht!", emailbody, sensitive['SENSITIVE_NORTHEAST_SMTPSERVER'], \
            sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], sensitive['SENSITIVE_NORTHEAST_SMTPPASSWORD'], sensitive['SENSITIVE_NORTHEAST_SMTPPORT'])
-
-	
-	
-	
 	return True
 
 def send_email(sender, to, subject, body, smtpserver, smtpuser, smtppassword, smtpport):
@@ -135,43 +102,31 @@ def send_email(sender, to, subject, body, smtpserver, smtpuser, smtppassword, sm
         msg['From'] = sender
         msg['To'] = to
         s = smtplib.SMTP(smtpserver)
-        s.sendmail(sender, to, msg.as_string())
-		
+        s.sendmail(sender, to, msg.as_string())	
         s.quit    
         return True
-		
-		
+			
 sensitive = read_sensitive("/var/www/.sensitive")
-	
 owm_api_key = sensitive['SENSITIVE_OPENWEATHERMAP_APIKEY']
 owm_api_lon = "8.2806"
 owm_api_lat = "48.7644"
 owm_api_baseurl = "http://api.openweathermap.org/data/2.5/forecast?"
 owm_api_requesturl = owm_api_baseurl + "lat=" + owm_api_lat + "&lon=" + \
                      owm_api_lon + "&appid=" + owm_api_key
-#print (owm_api_requesturl)
-
-
 
 jsonurl = urlopen(owm_api_requesturl)
 response = json.loads(jsonurl.read())
 forecast_list = response['list']
-# p#print(forecast)
 good_times = []
+
 for forecast in forecast_list:
 	date = datetime.fromtimestamp(int(forecast['dt']))
-	
 	if is_good_time_for_briefing(forecast):
 		print ("%s Looking good!" % date)
 		good_times.append(forecast)
-	
-		
-		
+
 if len(good_times) > 0:
 	print ("Sending email ...")
 	announce_briefing_day(good_times)
-	
 else:
 	print ("No days, no email.")
-
-
