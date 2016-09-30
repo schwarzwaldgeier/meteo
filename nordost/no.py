4c01e48b61d7c7cb830faadbdd6b00ea2be81c60
Sebastian Schmied <s.schmied@gmail.com>
2016-09-30 11:18:54 +0200

    made nordost/no.py pep8 compatible

--- nordost/no.py
+++ nordost/no.py
@@ -29,72 +29,72 @@ def read_sensitive(path):
     return sensitive
 
 def is_windspeed_ok(forecast):
-	wind_speed = forecast['wind']['speed']
-	if wind_speed <= max_windspeed:
-		return True
-	else:
-		return False
-		
+    wind_speed = forecast['wind']['speed']
+    if wind_speed <= max_windspeed:
+        return True
+    else:
+        return False
+        
 def is_wind_direction_ok(forecast):
-	wind_direction = forecast['wind']['deg']
-	north = wind_direction_boundaries['north']
-	east = wind_direction_boundaries['east']
-	if north <= wind_direction <= east:
-		return True
-	else:
-		return False
-	
+    wind_direction = forecast['wind']['deg']
+    north = wind_direction_boundaries['north']
+    east = wind_direction_boundaries['east']
+    if north <= wind_direction <= east:
+        return True
+    else:
+        return False
+    
 def is_weekend(forecast):
-	date = datetime.fromtimestamp(int(forecast['dt']))
-	if date.weekday() >= 5:
-		return True
-	else:
-		return False;
-		
+    date = datetime.fromtimestamp(int(forecast['dt']))
+    if date.weekday() >= 5:
+        return True
+    else:
+        return False;
+        
 def is_dry(forecast):
-	if not forecast['rain']: # would contain downpour in l/m2 otherwise
-		return True
-	else:
-		return False
-	
+    if not forecast['rain']: # would contain downpour in l/m2 otherwise
+        return True
+    else:
+        return False
+    
 def is_sunny(forecast):
-	if int(forecast['clouds']['all']) <= 75: #=6/8 clouds max
-		return True
-	else:
-		return False
-	
-	
+    if int(forecast['clouds']['all']) <= 75: #=6/8 clouds max
+        return True
+    else:
+        return False
+    
+    
 def is_during_day(forecast):
-	date = datetime.fromtimestamp(forecast['dt'])
-	if 10 <= date.hour <= 17:
-		return True
-	else:
-		return False
-	
+    date = datetime.fromtimestamp(forecast['dt'])
+    if 10 <= date.hour <= 17:
+        return True
+    else:
+        return False
+    
 def is_good_time_for_briefing(forecast):
-	if is_weekend(forecast) and is_during_day(forecast) and is_wind_direction_ok(forecast) and is_windspeed_ok and is_dry(forecast) and is_sunny(forecast):
-		return True
-	else:
-		return False
+    if is_weekend(forecast) and is_during_day(forecast) and is_wind_direction_ok(forecast) and is_windspeed_ok and is_dry(forecast) and is_sunny(forecast):
+        return True
+    else:
+        return False
 
 def announce_briefing_day(forecast):
 
-	formatted_forecasts = ""
-	German_weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'] #yes i know there is something built in for this but it looked complicated and I didn't care
-	for f in forecast:
-		godddate = date.fromtimestamp(f['dt'])
-		windspeed = str(int(round(f['wind']['speed'] * 3.6)))
-		winddirection = str(int(round(f['wind']['deg'])))
-		clouds = str(int(round(float(f['clouds']['all']) / 12.5))) #convert to 1/8s
-		formatted_forecasts = formatted_forecasts + German_weekdays[date.weekday()] + ", " + str(godddate.day) + '.' + str(godddate.month) + '.' + str(godddate.year) + ' ' + str(godddate.hour) + ':' + str(godddate.minute).rjust(2, '0') + ' Uhr:\n' + windspeed + ' km/h aus ' + winddirection + '° NO, ' + clouds + '/8 Bewölkung, kein Regen.\n\n'
-			
-	emailbody = open('/var/www/nordost/no-email.txt').read()
-	emailbody = emailbody.replace('{forecasts}', formatted_forecasts)
-	emailbody = emailbody.replace('{participantsurl}', sensitive['SENSITIVE_NORTHEAST_LISTURL'])
-	emailbody = emailbody.replace('{mailinglist}', sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'])
-	send_email(sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], "Nordosteinweiser für's Wochenende gesucht!", emailbody, sensitive['SENSITIVE_NORTHEAST_SMTPSERVER'], \
+    formatted_forecasts = ""
+    German_weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'] #yes i know there is something built in for this but it looked complicated and I didn't care
+    for f in forecast:
+        godddate = date.fromtimestamp(f['dt'])
+        windspeed = str(int(round(f['wind']['speed'] * 3.6)))
+        winddirection = str(int(round(f['wind']['deg'])))
+        clouds = str(int(round(float(f['clouds']['all']) / 12.5))) #convert to 1/8s
+        formatted_forecasts = formatted_forecasts + German_weekdays[date.weekday()] + ", " + str(godddate.day) + '.' + str(godddate.month) + '.' + str(godddate.year) + ' ' + str(godddate.hour) + ':' + str(godddate.minute).rjust(2, '0') + ' Uhr:\n' + windspeed + ' km/h aus ' + winddirection + '° NO, ' + clouds + '/8 Bewölkung, kein Regen.\n\n'
+            
+    emailbody = open('/var/www/nordost/no-email.txt').read()
+    emailbody = emailbody.replace('{forecasts}', formatted_forecasts)
+    emailbody = emailbody.replace('{participantsurl}', sensitive['SENSITIVE_NORTHEAST_LISTURL'])
+    emailbody = emailbody.replace('{mailinglist}', sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'])
+    send_email(sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], "Nordosteinweiser für's Wochenende gesucht!", emailbody, sensitive['SENSITIVE_NORTHEAST_SMTPSERVER'], \
            sensitive['SENSITIVE_NORTHEAST_BRIEFERSMAIL'], sensitive['SENSITIVE_NORTHEAST_SMTPPASSWORD'], sensitive['SENSITIVE_NORTHEAST_SMTPPORT'])
-	return True
+    return True
 
 def send_email(sender, to, subject, body, smtpserver, smtpuser, smtppassword, smtpport):
         msg = MIMEText(body)
@@ -102,7 +102,7 @@ def send_email(sender, to, subject, body, smtpserver, smtpuser, smtppassword, sm
         msg['From'] = sender
         msg['To'] = to
         s = smtplib.SMTP(smtpserver)
-        s.sendmail(sender, to, msg.as_string())	
+        s.sendmail(sender, to, msg.as_string())    
         s.quit    
         return True
 
@@ -122,14 +122,14 @@ forecast_list = response['list']
 good_times = []
 
 for forecast in forecast_list:
-	date = datetime.fromtimestamp(int(forecast['dt']))
-	if is_good_time_for_briefing(forecast):
-		print ("%s Looking good!" % date)
-		good_times.append(forecast)
+    date = datetime.fromtimestamp(int(forecast['dt']))
+    if is_good_time_for_briefing(forecast):
+        print ("%s Looking good!" % date)
+        good_times.append(forecast)
 
 if len(good_times) > 0:
-	print ("Sending email ...")
-	announce_briefing_day(good_times)
+    print ("Sending email ...")
+    announce_briefing_day(good_times)
 else:
-	print ("No good days, no email.")
+    print ("No good days, no email.")
 print ("---")
