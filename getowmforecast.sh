6cc7bc324632f40ee1d8fd225dcbcfdf864c76cf
Geier ROOT <whatever@schwarzwaldgeier.de>
2017-12-08 09:53:28 +0100

    rm nordost alert

--- getowmforecast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/bin/bash ./.sensitive
-url = "http://api.openweathermap.org/data/2.5/forecast?lat=48.7644&lon=8.2806&appid="
-url = $url + SENSITIVE_OPENWEATHERMAP_APIKEY
-echo "test"
-curl url > /var/www/owmforecast.json
